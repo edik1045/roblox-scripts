1f755d3e7a711eae2ec20224091552d12fc5e184
edik1045 <66965109+edik1045@users.noreply.github.com>
2022-03-03 10:43:10 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 8317588085 then
-  loadstring(game:HttpGet("http://raw.githubusercontent.com/edik1045/roblox-scripts/main/Block_Tycoon.lua"))()
+  loadstring(game:HttpGet("http://raw.githubusercontent.com/edik1045/roblox-scripts/main/Block Tycoon.lua"))()
 end
