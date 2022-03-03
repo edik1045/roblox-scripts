c25ba681982a3a78a68ff0ae54c357170ec0b7d6
edik1045 <66965109+edik1045@users.noreply.github.com>
2022-03-03 12:29:17 +0200

    Update Block Tycoon.lua

--- Block Tycoon.lua
+++ Block Tycoon.lua
@@ -26,18 +26,19 @@ local Blocks = {}
 
 
 --// UI Library \\--
-local Library = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Just-Egg-Salad/roblox-scripts/main/uwuware'))()
-local Window = Library:CreateWindow("Script by edik1045")
-Window:AddLabel({
-    text = "discord.gg/XUata5aXz9"
-})
-Window:AddToggle({
-    text = "Inf. Blocks",
-    flag = "Inf"
-})
-Window:AddToggle({
-    text = "All Blocks",
-    callback = function(A_1)
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/edik1045/roblox-scripts/main/UI%20lib.lua"))()
+local Window = Library:Create("edik1045 Hub", "Block Tycoon")
+local Page = Window:Page("Main", true)
+Page:Toggle("Inf. Blocks", function(Inf)
+    if Inf then
+            
+        Library.flags.Inf = true
+    else
+         Library.flags.Inf = false
+    end
+end
+)
+Page:Toggle("All Blocks", function(A_1)
         if A_1 == true then
             table.foreach(Blocks, function(A_1, A_2)
                 A_2.Parent = Inventory
@@ -48,48 +49,26 @@ Window:AddToggle({
             end)
         end
     end
-})
+)
+local e = Fake.Value
+window:Slider('Fake Blocks', 0, 100, function(value)
+    e = value
+end)
 
-local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/loglizzy/lib/main/main.lua'))()
-local window = lib:Window('Script By edik1045_mp3')
-window:Toggle('Inf. Blocks', function(Inf)
-    if Inf then
-        
-        Library.flags.Inf = true
-    else
-        Library.flags.Inf = false
-    end
-end
-)
-window:Toggle('All Blocks', function(A_1)
-        if A_1 then
-            table.foreach(Blocks, function(A_1, A_2)
-                A_2.Parent = Inventory
-            end)
-        else
-            table.foreach(Blocks, function(A_1, A_2)
-                A_2.Parent = nil
-            end)
-        end
-    end
-)
+
+
 
 --// Get Blocks \\--
 for _, A_1 in next, BlockFolder:GetChildren() do
     table.insert(Blocks, (function()
         local Fake = Instance.new("NumberValue")
         Fake.Name = A_1.Name
-        Fake.Value = value
+        Fake.Value = e
         return Fake
     end)())
 end
 
-window:Slider('Fake Blocks', {
-     max = 80,
-     def = 20
-}, function(value)
-    Fake.Value = value
-end)
+
 
 
 
